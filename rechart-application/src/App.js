eff65fa9d9be6618a430a9801a7d4181d956f17d
Kurubasaipraveen <154404195+Kurubasaipraveen@users.noreply.github.com>
2024-06-27 08:54:06 +0530

    App.js

--- rechart-application/src/App.js
+++ rechart-application/src/App.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import ChartComponent from './components/ChartComponent';
-
+import React from 'react'
+import ChartComponent from './components/ChartComponent'
+import './App.css'
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <h1>Chart Application</h1>
       <ChartComponent />
       <h4>Build By Kuruba saipraveen</h4>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
